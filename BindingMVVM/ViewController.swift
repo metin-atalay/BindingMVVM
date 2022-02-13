bcadcd6870c33fd138a3f2d8f120a94f3395fc91
Metin ATALAY <metin.atalay@architecht.com>
2022-02-13 19:31:23 +0300

    Initial Commit

---
+++ BindingMVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BindingMVVM
+//
+//  Created by Metin Atalay on 13.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
